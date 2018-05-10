ab05516a8ac9bd8c7ab600b6c13f5b4c00e022c7
Ermal Ferati <ermalferati@gmail.com>
2018-05-10 15:48:37 +0200

    First commit
    Added the source code

---
+++ Assets/Text Mesh Spawner/Scripts/SampleScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            int random = Random.Range(0, transform.childCount);
+            TextMeshSpawner.instance.spawnText("Hello", transform.GetChild(random).position, transform.GetChild(random).rotation);
+        }
+    }
+}
---
+++ Assets/Text Mesh Spawner/Scripts/TextMeshSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TextMeshSpawner : MonoBehaviour
+{
+    public static TextMeshSpawner instance;
+    public TextMesh textPrefab;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void spawnText(string text, Vector3 position, Quaternion rotation, float fadeSpeed = 0.5f, float moveSpeed = 3.0f)
+    {
+        StartCoroutine(spawnTextEnumerator(text, position, rotation, fadeSpeed, moveSpeed));
+    }
+
+    private IEnumerator spawnTextEnumerator(string text, Vector3 position, Quaternion rotation, float fadeSpeed = 0.5f, float moveSpeed = 3.0f)
+    {
+        TextMesh textObject = Instantiate(textPrefab, position, rotation) as TextMesh;
+        textObject.text = text;
+        while (textObject.color.a > 0f)
+        {
+            yield return new WaitForEndOfFrame();
+            Color c = textObject.color;
+            c.a -= Time.deltaTime * 0.5f;
+            textObject.color = c;
+            textObject.transform.Translate(Vector3.up * 3.0f * Time.deltaTime);
+        }
+        Destroy(textObject.gameObject);
+    }
+}
